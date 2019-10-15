c18ac74dbed5752890173748ec6d537be3c67511
Alan <alan.silva@imd.ufrn.br>
2019-10-15 17:48:33 -0300

    Fonts. Wire Front (wip)

---
+++ Sistema/fonts/colors.js
@@ -0,0 +1,16 @@
+colors = {
+  'transparent': 'transparent',
+
+  'black': '#2B4964',
+  'grey-darkest': '#626471',
+  'grey-darker': '#878c98',
+  'grey-dark': '#adb4c2',
+  'grey': '#d5d9e3',
+  'grey-light': '#dee1e8',
+  'grey-lighter': '#eaebef',
+  'grey-lightest': '#fcfcfc',
+  'white': '#ffffff',
+
+  'primary': '#2b79c1',
+  'primary-dark': '#266299'
+}
