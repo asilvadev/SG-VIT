db8124e78bd138626a37813e4f4fcccd4605f13c
Alan <alan.silva@imd.ufrn.br>
2019-10-23 17:53:53 -0300

    func e infos TO-DO

--- Sistema/epass/backend/src/controllers/IngressoController.js
+++ Sistema/epass/backend/src/controllers/IngressoController.js
@@ -44,4 +44,6 @@ module.exports = {
 
         return res.json(ingresso);
     }
+
+    
 }
--- Sistema/epass/backend/src/controllers/UserController.js
+++ Sistema/epass/backend/src/controllers/UserController.js
@@ -9,9 +9,9 @@ module.exports = {
     },
 
     async store(req, res){
-        const { name, email, cpf, senha, is_func} = req.body;
+        const { name, email, cpf, senha, is_func, is_admin} = req.body;
 
-        const user = await User.create({name, email, cpf, senha, is_func});
+        const user = await User.create({name, email, cpf, senha, is_func, is_admin});
 
         return res.json(user);
     }
