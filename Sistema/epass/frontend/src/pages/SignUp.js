98fceda9080672f9c3c36c0314f5a332db8e3f5e
Alan Silva <alan.silva@imd.ufrn.br>
2019-10-25 00:06:44 -0300

    estilos de borda quando selecionado. fix endentadura

--- Sistema/epass/frontend/src/pages/SignUp.js
+++ Sistema/epass/frontend/src/pages/SignUp.js
@@ -3,49 +3,48 @@ import './SignUp.css';
 
 import logo from '../assets/epass.png';
 
-export default function Login(){
+export default function Login() {
 
-    const [nome, setNome] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [cpf, setCPF] = React.useState('');
-    const [password, setPassword] = React.useState('');
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [cpf, setCPF] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-    function handlesSubmit(e){
-        e.preventDefault();
+  function handlesSubmit(e) {
+    e.preventDefault();
 
-        console.log(email);
-        console.log(password);
-    }
+    console.log(email);
+    console.log(password);
+  }
 
+  //Pode remover os value (para não aparecer no html)
+  return (
+    <div className="login-container">
+      <img src={logo} alt="ePass" />
+      <form onSubmit={handlesSubmit}>
 
-    return (
-        <div className="login-container">
-            <img src={logo} alt="ePass" />
-            <form onSubmit={handlesSubmit}>
-                
-                <input type="text" placeholder="Nome" 
-                value={nome}
-                onChange={e => setNome(e.target.value)}
-                />
-               
-                <input type="text" placeholder="CPF" maxlength="11"
-                value={cpf}
-                onChange={e => setCPF(e.target.value)}
-                />
+        <input type="text" placeholder="Nome"
+          value={nome}
+          onChange={e => setNome(e.target.value)}
+        />
 
-                <input type="text" placeholder="E-mail" 
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                />
+        <input type="text" placeholder="CPF" maxlength="11"
+          value={cpf}
+          onChange={e => setCPF(e.target.value)}
+        />
 
-                <input type="password" placeholder="Senha" 
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                />
-                <button type="submit">Cadastrar</button>
-            </form>
-        </div>
+        <input type="text" placeholder="E-mail"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
 
-    );
+        <input type="password" placeholder="Senha"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
 }
 
