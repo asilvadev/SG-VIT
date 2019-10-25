9f92eee60cf37d517e9c0e046b1ce4536f3d327b
Alan Silva <alan.silva@imd.ufrn.br>
2019-10-25 00:07:17 -0300

    adicionado validador de blank input. validacao de unic email/cpf (w.i.p)

--- Sistema/epass/backend/src/controllers/UserController.js
+++ Sistema/epass/backend/src/controllers/UserController.js
@@ -11,6 +11,61 @@ module.exports = {
     async store(req, res){
         const { name, email, cpf, senha, is_func, is_admin} = req.body;
 
+        /* VALIDACAO */
+        if(!name){
+          return res.send({
+            success: false,
+            message: 'Error: Name cannot be blank.'
+          });
+        }
+        else if(!email){
+          return res.send({
+            success: false,
+            message: 'Error: E-mail cannot be blank.'
+          });
+        }
+        else if(!cpf){
+          return res.send({
+            success: false,
+            message: 'Error: CPF cannot be blank.'
+          });
+        }
+        else if(!senha){
+          return res.send({
+            success: false,
+            message: 'Error: Senha cannot be blank.'
+          });
+        }
+
+        const check_email = await User.findAll({
+          where : {
+            email
+          }
+        });
+        if(!check_email){
+
+          return res.json(check_email);
+        }
+        return res.json(check_email);
+
+
+
+
+        // return res.status(400).json({ error: 'Show not found!' });
+        // if(!check_email){
+        //   return res.send({
+        //     success: false,
+        //     message: 'Error: E-mail already exist.'
+        //   });
+        // }
+        // const cpf_check = await User.findAll(cpf.body);
+        // if(!cpf_check){
+        //   return res.send({
+        //     success: false,
+        //     message: 'Error: CPF already exist.'
+        //   });
+        // }
+
         const user = await User.create({name, email, cpf, senha, is_func, is_admin});
 
         return res.json(user);
