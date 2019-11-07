7e00fef64f30f2f4a46b4185f0d463b00fad8a46
Alan <alan.silva@imd.ufrn.br>
2019-11-07 16:31:09 -0300

    select e files setStatus

--- Sistema/epass/frontend/src/pages/ShowCreate.js
+++ Sistema/epass/frontend/src/pages/ShowCreate.js
@@ -1,39 +1,29 @@
-import React from 'react';
-import './ShowCreat.css';
+import React from "react";
+import "./ShowCreat.css";
 
-import api from '../services/api';
+import api from "../services/api";
 
-import logo from '../assets/epass.png';
+import logo from "../assets/epass.png";
 
-export default function ShowCreat({history}) {
+export default function ShowCreat({ history }) {
+  const [name, setName] = React.useState();
+  const [sinopse, setSinopse] = React.useState();
+  const [director, setDirector] = React.useState();
+  const [duration, setDuration] = React.useState();
+  const [classificacao, setClassificacao] = React.useState();
+  const [genero, setGenero] = React.useState();
+  const [price, setPrice] = React.useState();
+  const [image, setImage] = React.useState();
 
-  /*
-   name: DataTypes.STRING,
-            sinopse: DataTypes.TEXT,
-            director: DataTypes.STRING,
-            duration: DataTypes.TIME,
-            classificacao: DataTypes.ENUM(
-              'Livre',
-              'Adulto'
-              ),
-            genero : DataTypes.STRING,
-            price: DataTypes.INTEGER,
-            image : DataTypes.STRING,
-            */
-
-  const [name, setName] = React.useState('');
-  const [sinopse, setSinopse] = React.useState('');
-  const [director, setDirector] = React.useState('');
-  const [duration, setDuration] = React.useState('');
-  const [classificacao, setClassificacao] = React.useState('');
-  const [genero, setGenero] = React.useState('');
-  const [price, setPrice] = React.useState('');
-  const [image, setImage] = React.useState('');
+  var options = [
+    { value: "Livre", label: "Livre" },
+    { value: "Adulto", label: "Adulto" }
+  ];
 
   async function handlesSubmit(e) {
     e.preventDefault();
 
-    const response = api.post('/show/create', {
+    const response = api.post("/show/create", {
       name,
       sinopse,
       director,
@@ -52,57 +42,85 @@ export default function ShowCreat({history}) {
     <div className="show-create-container">
       <img src={logo} alt="ePass" />
       <form onSubmit={handlesSubmit}>
-
-        <input type="text" placeholder="Nome da Peça"
+        <input
+          required
+          type="text"
+          placeholder="Nome da Peça"
           value={name}
           onChange={e => setName(e.target.value)}
         />
-        <textarea type="text" placeholder="Sinopse"
+        <textarea
+          required
+          type="text"
+          placeholder="Sinopse"
           value={sinopse}
-
           onChange={e => setSinopse(e.target.value)}
         />
-        <input type="text" placeholder="Diretor"
+        <input
+          required
+          type="text"
+          placeholder="Diretor"
           value={director}
           onChange={e => setDirector(e.target.value)}
         />
-        <input type="time" placeholder="Duracao"
+        <input
+          required
+          type="time"
+          placeholder="Duracao"
           value={duration}
-
           onChange={e => setDuration(e.target.value)}
         />
-        <input type="enum" placeholder="Livre ou Adulto"
+        {/* <input
+          type="enum"
+          placeholder="Livre ou Adulto"
           value={classificacao}
-
           onChange={e => setClassificacao(e.target.value)}
-        />
-        {/* <select name="classificacao" value="Livre">
-          <option value="'Livre'"
-            onChange={e => setClassificacao(e.target.value)}>Livre</option>
-          <option value='Adulto'
-            onChange={e => setClassificacao(e.target.value)}>Adulto</option>
+        /> */}
+        {/* <select name="classificacao">
+          <option value="Livre" onChange={e => setClassificacao("Livre")}>
+            Livre
+          </option>
+          <option value="Adulto" onChange={e => setClassificacao("Adulto")}>
+            Adulto
+          </option>
         </select> */}
 
+        <select
+          required
+          name="classificacao-select"
+          defaultValue="Livre"
+          value={classificacao}
+          onChange={e => setClassificacao(e.target.value)}
+        >
+          <option defaultValue="Livre" value="Livre">
+            Livre
+          </option>
+          <option value="Adulto">Adulto</option>
+        </select>
 
-        <input type="text" placeholder="Genero"
+        <input
+          required
+          type="text"
+          placeholder="Genero"
           value={genero}
           onChange={e => setGenero(e.target.value)}
         />
 
-        <input type="number" placeholder="Valor"
+        <input
+          required
+          type="number"
+          placeholder="Valor"
           value={price}
           onChange={e => setPrice(e.target.value)}
         />
-        <input type="file"
-          value={image}
-          onChange={e => setImage(e.target.value)}
+        <input
+          required
+          type="file"
+          onChange={e => setImage(e.target.files[0])}
         />
-
-
 
         <button type="submit">Cadastrar</button>
       </form>
     </div>
   );
 }
-
