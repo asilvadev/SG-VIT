c162773b13f6a65770760afb5bcfbdd75b5dbb14
Alan <alan.silva@imd.ufrn.br>
2019-11-11 15:03:05 -0300

    #5 backend date control FIX (search in db)

--- Sistema/epass/backend/src/controllers/UserController.js
+++ Sistema/epass/backend/src/controllers/UserController.js
@@ -1,70 +1,91 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 module.exports = {
+  async index(req, res) {
+    const users = await User.findAll();
 
-    async index(req, res){
-        const users = await User.findAll();
+    return res.json(users);
+  },
 
-        return res.json(users);
-    },
+  async store(req, res) {
+    const { name, email, cpf, senha, is_func, is_admin } = req.body;
 
-    async store(req, res){
-        const { name, email, cpf, senha, is_func, is_admin} = req.body;
-
-        /* VALIDACAO */
-        if(!name){
-          return res.send({
-            success: false,
-            message: 'Error: Name cannot be blank.'
-          });
-        }
-        else if(!email){
-          return res.send({
-            success: false,
-            message: 'Error: E-mail cannot be blank.'
-          });
-        }
-        else if(!cpf){
-          return res.send({
-            success: false,
-            message: 'Error: CPF cannot be blank.'
-          });
-        }
-        else if(!senha){
-          return res.send({
-            success: false,
-            message: 'Error: Senha cannot be blank.'
-          });
-        }
-        else if(cpf.length < 11){
-          return res.send({
-            success: false,
-            message: 'Error: CPF length invalid.'
-          });
-        }
-        
-        
-
-
-
-
-        // return res.status(400).json({ error: 'Show not found!' });
-        // if(!check_email){
-        //   return res.send({
-        //     success: false,
-        //     message: 'Error: E-mail already exist.'
-        //   });
-        // }
-        // const cpf_check = await User.findAll(cpf.body);
-        // if(!cpf_check){
-        //   return res.send({
-        //     success: false,
-        //     message: 'Error: CPF already exist.'
-        //   });
-        // }
-
-        const user = await User.create({name, email, cpf, senha, is_func, is_admin});
-
-        return res.json(user);
+    /* VALIDACAO */
+    if (!name) {
+      return res.send({
+        success: false,
+        message: "Error: Name cannot be blank."
+      });
+    } else if (!cpf) {
+      return res.send({
+        success: false,
+        message: "Error: CPF cannot be blank."
+      });
+    } else if (!senha) {
+      return res.send({
+        success: false,
+        message: "Error: Senha cannot be blank."
+      });
+    } else if (cpf.length < 11 || cpf.length > 12) {
+      return res.send({
+        success: false,
+        message: "Error: CPF length invalid."
+      });
+    } else if (!email) {
+      return res.send({
+        success: false,
+        message: "Error: Email cannot be blank."
+      });
     }
-}
+    const bd_email = await User.findOne({
+      where: {
+        email: email
+      }
+    });
+
+    if (bd_email) {
+      return res.send({
+        success: false,
+        message: "Error: Email is alredy registed"
+      });
+    }
+    const bd_cpf = await User.findOne({
+      where: {
+        cpf: cpf
+      }
+    });
+
+    if (bd_cpf) {
+      return res.send({
+        success: false,
+        message: "Error: CPF is alredy registed"
+      });
+    }
+
+    // return res.status(400).json({ error: 'Show not found!' });
+    // if(!check_email){
+    //   return res.send({
+    //     success: false,
+    //     message: 'Error: E-mail already exist.'
+    //   });
+    // }
+    // const cpf_check = await User.findAll(cpf.body);
+    // if(!cpf_check){
+    //   return res.send({
+    //     success: false,
+    //     message: 'Error: CPF already exist.'
+    //   });
+    // }
+
+    const user = await User.create({
+      name,
+      email,
+      cpf,
+      senha,
+      is_func,
+      is_admin
+    });
+
+    return res.json(user);
+  }
+};
