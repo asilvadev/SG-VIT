1563d9331f71477987aa36749974544adf49aa86
Alan Silva <alan.silva@imd.ufrn.br>
2019-11-13 08:03:44 -0300

    #4 .home inciando list view

--- Sistema/epass/frontend/src/pages/Home.js
+++ Sistema/epass/frontend/src/pages/Home.js
@@ -17,6 +17,7 @@ export default function Home({ history }) {
 
 
       setPecas(response.data);
+      console.log(response.data)
     }
     loadPecas();
   }, []);
@@ -26,7 +27,8 @@ export default function Home({ history }) {
 
   return (
 
-    <Slider className="slider-wrapper" autoPlay duration={6000}>
+    <div className="externo">
+<Slider className="slider-wrapper" autoPlay duration={6000}>
       {pecas.map((peca => (
         <div
           key={peca.id}
@@ -42,11 +44,42 @@ export default function Home({ history }) {
 
           </div>
         </div>
-      )))}
+
+)))}
     </Slider>
 
+{/* {pecas.map((peca=>(
 
-  );
+)))} */}
+<div className="cont-widget">
+<div class="item card-evento slick-slide slick-current slick-active" id="portal-card-event-8571"  tabindex="0" data-slick-index="0" aria-hidden="false">
+	<a class="link" href="https://uhuu.com/evento/rs/porto-alegre/luccas-neto-com-os-aventureiros-8571" tabindex="0">
+  <span class="container-img">
+    <img class="lozad" src="https://imagens.uhuu.com/imgs/17287-510x310-imgproguhuu-luccasneto-1.jpg" alt=""></img>
+  </span>
+				<div class="card-info">
+				<div class="infoCardDesk hidden-md hidden-lg">
+				<span class="evento-cidade">Porto Alegre/RS</span>
+				<span class="evento-nome" title="Luccas Neto com Os Aventureiros">
+													Luccas Neto com Os Aventureir...
+										</span>
+		</div>
+			<div class="info-evento">
+			 					<div class="minicard-data">
+						<span class="data-semana">Sex</span>
+						<span class="data-layer">15</span>
+						<span class="data-mes">NOV</span>
+					</div>
+
+
+				<span class="local-nome">Ginásio Gigantinho</span>
+			</div>
+		</div>
+	</a>
+</div>
+  </div>
+
+    </div>);
 }
 
 
